6919f0ac2ae110034cf12be42795d388bc1ce8eb
Fabiana Tellechea <85513647+fabianatellechea@users.noreply.github.com>
2024-01-06 22:30:46 +0000

    added: task 0 & README

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """async_generator function"""
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
