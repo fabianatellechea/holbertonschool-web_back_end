9552fc5592e8e6aa2f90ae17915d37ff2a9f6d09
Fabiana Tellechea <85513647+fabianatellechea@users.noreply.github.com>
2024-01-06 23:06:54 +0000

    added: task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Run time for four parallel comprehensions"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure_runtime function"""
+    start = time.time()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    end = time.time()
+    return end - start
